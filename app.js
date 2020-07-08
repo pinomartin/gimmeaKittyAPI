5eedb016efb71dcbf479ae81eab720c1bb03c564
pinomartinUTN <110933@tecnicatura.frc.utn.edu.ar>
2020-07-08 01:19:01 -0300

    Upd

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const boton = document.getElementById('btn-kitty');
-
+const saludo = document.getElementById('greeting');
+console.log(saludo)
 
 const peticion = `https://api.thecatapi.com/v1/images/search`;
+
+// https://api.thecatapi.com/images/search
 const apiKEY = '78711a1f-0db0-4bdf-b231-1cc94bc3115f';
 
 const imagen = document.getElementById('imagen')
 const svg = 'svg/catspinner.svg';
+
 const getKitties = async () => {
-    
     
     try{
 
         imagen.src = `${svg}`;
+        saludo.innerText = "";
         const respuesta = await fetch(peticion, { headers: { 'x-api-key':`${apiKEY}`} });
         const data = await respuesta.json();
+        console.log(data)
         const {url} = data[0];
         
+        setTimeout(() => {
+            saludo.innerText = 'Hi Human !';
+
+        },1000);
+        
         imagen.src = url;
-        
-        
+
         
     }catch (error){
 
