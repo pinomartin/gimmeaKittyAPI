5f693fbf91474e6ae6c20a43f78b82a4e38cc649
pinomartinUTN <110933@tecnicatura.frc.utn.edu.ar>
2020-07-07 21:48:09 -0300

    Add catSpinner

--- app.js
+++ app.js
@@ -5,13 +5,14 @@ const peticion = `https://api.thecatapi.com/v1/images/search`;
 const apiKEY = '78711a1f-0db0-4bdf-b231-1cc94bc3115f';
 
 const imagen = document.getElementById('imagen')
-
+const svg = 'svg/catspinner.svg';
 const getKitties = async () => {
     
     
     try{
 
-        const respuesta = await fetch(peticion);
+        imagen.src = `${svg}`;
+        const respuesta = await fetch(peticion, { headers: { 'x-api-key':`${apiKEY}`} });
         const data = await respuesta.json();
         const {url} = data[0];
         
